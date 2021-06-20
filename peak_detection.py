3b01a11ef728d637a393140c4f5fd7b0ff0ca0e9
David Fong <davidgfong@gmail.com>
2021-06-19 18:50:11 -0700

    Low Pass Filter
    Low Pass Filter

--- peak_detection.py
+++ peak_detection.py
@@ -8,17 +8,28 @@ Created on Sat Jun 19 09:36:33 2021
 import plotly.graph_objects as go
 import pandas as pd
 from scipy.signal import find_peaks
+from scipy.signal import butter, lfilter
 
 col_names = ['Data No',
              'Image Energy',
              'Last Average',
              'Time']
 
-walabot_data = pd.read_csv('walabot.csv', names = col_names, skiprows=[0])
+walabot_data = pd.read_csv('walabot_fast.csv', names = col_names, skiprows=[0])
 
 time_series = walabot_data['Last Average']
 
-indices = find_peaks(time_series, distance=20, prominence=(0.002, 1), height=0.05 )[0]
+fs = 157.08 #sampling frequency
+nyq = fs / 2
+high_cut = 12.5663706 #high frequency
+high = high_cut / nyq
+b, a = butter(7, high , btype='low')
+
+lp_filter_data = lfilter(b, a, time_series)
+
+print("Low Pass Filter: ", lp_filter_data)
+
+indices = find_peaks(lp_filter_data, distance=20, prominence=(0.002, 1), height=0.05)[0]
 
 print("Peak Indices :", indices)
 print("Number of Peaks :", len(indices))
@@ -36,12 +47,18 @@ fig = go.Figure()
 fig.add_trace(go.Scatter(
     y=time_series,
     mode='lines+markers',
-    name='Original Plot'
+    name='Original Plot (Rolling 10 Average)'
+))
+
+fig.add_trace(go.Scatter(
+    y=lp_filter_data,
+    mode='lines+markers',
+    name='LP Filter'
 ))
 
 fig.add_trace(go.Scatter(
     x=indices,
-    y=[time_series[j] for j in indices],
+    y=[lp_filter_data[j] for j in indices],
     mode='markers',
     marker=dict(
         size=8,
